f825cf998f8483a8fc6e1d2c31a866b4bc69de01
Edward Zota <zotaedward@gmail.com>
2023-02-21 13:29:23 +0100

    esercizio concluso

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const myName = prompt ('Inserisci il tuo nome');
+
+const mySurname = prompt('Inserisci ora il tuo cognome');
+
+const myFavoriteColor = prompt ('Adesso inserisci il tuo colore preferito');
+
+const actualYear = 23 ;
+
+document.getElementById('new_password').innerHTML = `La tua nuova (insicurissima)password è: ${myName}${mySurname}${myFavoriteColor}${actualYear} `
